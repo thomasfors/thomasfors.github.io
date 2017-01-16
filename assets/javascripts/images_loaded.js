b57864e441cbd81b333b8e6fd39f97882d90df57
Andree Ray <info@andreeray.se>
2017-01-16 16:12:36 +0100

    F

--- assets/javascripts/images_loaded.js
+++ assets/javascripts/images_loaded.js
@@ -1,12 +1,12 @@
-$(window).on("load", function() {
-    $( ".loading" ).animate({
-       opacity: 0
-   }, 500, function() {
-      $('html, body').css('overflowY', 'auto');
-      document.body.addEventListener('touchmove', function(e){ return true; });
-      document.ontouchmove = function(e){ return true; }
-     }).remove();
-});
+// $(window).on("load", function() {
+//     $( ".loading" ).animate({
+//        opacity: 0
+//    }, 500, function() {
+//       $('html, body').css('overflowY', 'auto');
+//       document.body.addEventListener('touchmove', function(e){ return true; });
+//       document.ontouchmove = function(e){ return true; }
+//      }).remove();
+// });
 
 
 document.ontouchmove = function(e){ e.preventDefault(); }
