96c34185602aba0e9e79d584567aa29ae04ebe80
Andree Ray <info@andreeray.se>
2017-01-17 11:32:14 +0100

    f

--- assets/javascripts/layout.js
+++ assets/javascripts/layout.js
@@ -3,6 +3,11 @@ var height = "innerHeight" in window
                ? window.innerHeight
                : document.documentElement.offsetHeight;
 
+
+
 $('.section--first,.section-backgroundImage').height(height)
-$('.section--frontpage').height("15rem")
+height = height - ($('.block').height() /2 +40)
+$('.section--frontpage').height(height)
+$('.textLogo--frontpage').css('line-height',height+"px");
+console.log()
 })
